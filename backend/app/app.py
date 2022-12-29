365523b31db77aa533417f798c098d6c554b0187
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2022-12-29 18:32:39 -0500

    Changed models.py database connector and renamed Server directory to backend

---
+++ backend/app/app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+from flask_cors import CORS
+from models import *
+import hashlib
+
+app = Flask(__name__)
+
+CORS(app)
+
+app.secret_key = os.urandom(32)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        pass
+
+    if request.method == 'POST':
+        name = request.form['name']
+        post = request.form['post']
+        create_post(name, post)
+
+    posts = get_posts()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/gift_cards', methods=['GET', 'POST'])
+def gift_cards():
+    if request.method == 'GET':
+        pass
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        email = data['email']
+        password = data['password']
+        amount = data['amount']
+        create_gift_card(int(amount), email, hashlib.sha256(password.encode('ascii')).hexdigest())
+    return {'200': 'Created Successfully'}
+
+
+@app.route('/redeem_cards', methods=['GET', 'POST'])
+def redeem_cards():
+    if request.method == 'GET':
+        pass
+    
+    if request.method == 'POST':
+        card_number = request.form.get('number')
+        email = request.form.get('email')
+        redeem_gift_card(card_number, email)
+
+    gift_cards = get_all_gift_cards()
+    return render_template('redeem_cards.html', posts=gift_cards)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    if request.method == 'GET':
+        pass
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        name = data['name']
+        email = data['email']
+        password = data['password']
+        create_user(name, email, hashlib.sha256(password.encode('ascii')).hexdigest())
+    return {'200': 'Registered Successfully'}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def user_login():
+    if request.method == 'GET':
+        pass
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = login(email, hashlib.sha256(password.encode('ascii')).hexdigest())
+        if user is not None:
+            session['user'] = user
+            return render_template('login.html', posts=user)
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True)
---
+++ backend/app/schema.sql
@@ -0,0 +1,23 @@
+drop table if exists posts;
+    create table posts(
+        id integer primary key AUTO_INCREMENT,
+        name text not null,
+        content text not null
+    );
+
+drop table if exists gift_cards;
+    create table gift_cards(
+        id integer primary key AUTO_INCREMENT,
+        user_email text not null foreign key references users(email),
+        card_number integer not null,
+        amount real not null
+    );
+
+drop table if exists users;
+    create table users (
+        user_id integer AUTO_INCREMENT primary key not null,
+        name text not null,
+        email text not null,
+        password text not null,
+        wallet real not null
+    );
