98b4252962aff6a865fb0e1eb8806654539c1615
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2023-01-01 22:07:29 -0500

    Transfer between VM and OS :(

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,5 +1,5 @@
 import hashlib
 
 def salt_password(user_id: str, password: str):
-    salt:str = user_id + password
+    salt:str = str(user_id) + str(password)
     return hashlib.sha256(salt.encode('ascii')).hexdigest()
