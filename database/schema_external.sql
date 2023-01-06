aa181c019661e491a2c216a1054f006e882ae7ac
SaraBuu <sara.aguincha@gmail.com>
2023-01-06 15:35:11 +0000

    mini change

--- database/schema_external.sql
+++ database/schema_external.sql
@@ -7,6 +7,6 @@ drop table if exists cupons;
 
 
 -- Populate cupons table
-insert into cupons (email, points) values ('francisco.m.c.sousa@gmail.com', 150);
-insert into cupons (email, points) values ('sara.aguincha@gmail.com', 150);
+insert into cupons (email, points) values ('francisco.m.c.sousa@gmail.com', 300);
+insert into cupons (email, points) values ('sara.aguincha@gmail.com', 200);
 insert into cupons (email, points) values ('miguel.porfirio@gmail.com', 150);
